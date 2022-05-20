d6c61604646e827fb7d7841a453e821ccf03c661
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2022-05-20 19:54:21 +0200

    port away from QStringList::toSet
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/folderwatcher.cpp
+++ src/gui/folderwatcher.cpp
@@ -157,7 +157,7 @@ void FolderWatcher::changeDetected(const QStringList &paths)
     //   - why do we skip the file altogether instead of e.g. reducing the upload frequency?
 
     // Check if the same path was reported within the last second.
-    QSet<QString> pathsSet = paths.toSet();
+    QSet<QString> pathsSet = QSet<QString>{paths.begin(), paths.end()};
     if (pathsSet == _lastPaths && _timer.elapsed() < 1000) {
         // the same path was reported within the last second. Skip.
         return;

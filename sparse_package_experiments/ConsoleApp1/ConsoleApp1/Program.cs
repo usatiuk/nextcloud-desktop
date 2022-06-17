131ece64fe546680a74368f7965d9f3de65f29f1
alex-z <blackslayer4@gmail.com>
2022-06-17 19:57:08 +0300

    Sparse package experiments.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

---
+++ sparse_package_experiments/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Management.Deployment;
+using System.Threading;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            string externalLocation = @"C:\Program Files\Nextcloud";
+            string sparsePkgPath = @"D:\work\nextcloud\sparse-package-test\SparsePackageTest.msix";
+
+           // removeSparsePackage();
+
+            //return;
+
+            //Attempt registration
+            if (registerSparsePackage(externalLocation, sparsePkgPath))
+            {
+                
+                Debug.WriteLine("Registration succeded, restart the app to run with identity");
+
+            }
+            else //Registration failed, run without identity
+            {
+                Debug.WriteLine("Package Registation failed, running WITHOUT Identity");
+            }
+
+        }
+
+        private static bool registerSparsePackage(string externalLocation, string sparsePkgPath)
+        {
+            bool registration = false;
+            try
+            {
+                Uri externalUri = new Uri(externalLocation);
+                Uri packageUri = new Uri(sparsePkgPath);
+
+                Console.WriteLine("exe Location {0}", externalLocation);
+                Console.WriteLine("msix Address {0}", sparsePkgPath);
+
+                Console.WriteLine("  exe Uri {0}", externalUri);
+                Console.WriteLine("  msix Uri {0}", packageUri);
+
+                PackageManager packageManager = new PackageManager();
+
+                //Declare use of an external location
+                var options = new AddPackageOptions();
+                options.ExternalLocationUri = externalUri;
+
+                Windows.Foundation.IAsyncOperationWithProgress<DeploymentResult, DeploymentProgress> deploymentOperation = packageManager.AddPackageByUriAsync(packageUri, options);
+
+                ManualResetEvent opCompletedEvent = new ManualResetEvent(false); // this event will be signaled when the deployment operation has completed.
+
+                deploymentOperation.Completed = (depProgress, status) => { opCompletedEvent.Set(); };
+
+                Console.WriteLine("Installing package {0}", sparsePkgPath);
+
+                Debug.WriteLine("Waiting for package registration to complete...");
+
+                opCompletedEvent.WaitOne();
+
+                if (deploymentOperation.Status == Windows.Foundation.AsyncStatus.Error)
+                {
+                    Windows.Management.Deployment.DeploymentResult deploymentResult = deploymentOperation.GetResults();
+                    Debug.WriteLine("Installation Error: {0}", deploymentOperation.ErrorCode);
+                    Debug.WriteLine("Detailed Error Text: {0}", deploymentResult.ErrorText);
+
+                }
+                else if (deploymentOperation.Status == Windows.Foundation.AsyncStatus.Canceled)
+                {
+                    Debug.WriteLine("Package Registration Canceled");
+                }
+                else if (deploymentOperation.Status == Windows.Foundation.AsyncStatus.Completed)
+                {
+                    registration = true;
+                    Debug.WriteLine("Package Registration succeeded!");
+                }
+                else
+                {
+                    Debug.WriteLine("Installation status unknown");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AddPackageSample failed, error message: {0}", ex.Message);
+                Console.WriteLine("Full Stacktrace: {0}", ex.ToString());
+
+                return registration;
+            }
+
+            return registration;
+        }
+
+        private static void removeSparsePackage() //example of how to uninstall a Sparse Package
+        {
+            PackageManager packageManager = new PackageManager();
+            Windows.Foundation.IAsyncOperationWithProgress<DeploymentResult, DeploymentProgress> deploymentOperation = packageManager.RemovePackageAsync("SparsePackageTest_1.0.0.0_x64__y5g7dx9hzfa18");
+            ManualResetEvent opCompletedEvent = new ManualResetEvent(false); // this event will be signaled when the deployment operation has completed.
+
+            deploymentOperation.Completed = (depProgress, status) => { opCompletedEvent.Set(); };
+
+            Debug.WriteLine("Uninstalling package..");
+            opCompletedEvent.WaitOne();
+        }
+    }
+}
